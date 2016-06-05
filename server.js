d7004ea3d8989ab624feed85498b7ac9a879bb22
Eric Hodges <ehodges2004@gmail.com>
2016-06-05 13:14:24 -0700

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require ('express');
 
 // create app
 var app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8500;
 
 app.use(function (req, res, next) {
 	if (req.headers ['x-forwarded-proto'] === 'https') {
